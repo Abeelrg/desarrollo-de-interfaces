067ea7ae1bcb1f57e71b5440344840dd49cce2b9
Abeelrg <abelriquelmegomezcenec@gmail.com>
2022-11-14 13:09:08 +0100

    Pruebas Scene Builder

---
+++ prSceneBuilder/src/application/SampleController.java
@@ -0,0 +1,26 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SampleController {
+
+	
+	@FXML
+	private TextField txtMensaje;
+	
+	@FXML
+	private Button btnEnviar;
+	
+	@FXML
+	private Label lblMensaje;
+	
+	@FXML
+	private void mostrarMensaje(ActionEvent event) {
+		lblMensaje.setText(txtMensaje.getText());
+	}
+	
+}
