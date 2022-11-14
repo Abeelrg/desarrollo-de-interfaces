676232ddea0f54fab42fe572588dfe154a47025b
Abeelrg <abelriquelmegomezcenec@gmail.com>
2022-11-14 13:27:39 +0100

    Ejercicio 1 Inicio Sesion - CENEC

---
+++ prSceneCenec/src/application/SampleController.java
@@ -0,0 +1,32 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SampleController {
+
+	
+	@FXML
+	private TextField txtNombre;
+	
+	@FXML
+	private PasswordField pswContrasena;
+	
+	@FXML
+	private Button btnEntrar;
+	
+	@FXML
+	private Label lblMensaje;
+	
+	@FXML
+	private void mostrarMensaje(ActionEvent event) {
+		lblMensaje.setText("Bienvenido " + txtNombre.getText());
+		txtNombre.clear();
+		pswContrasena.clear();
+	}
+	
+}
