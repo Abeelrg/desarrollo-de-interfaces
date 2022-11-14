851f8499d81c86d3e132c8a18755dc57b1f8a50c
Abeelrg <abelriquelmegomezcenec@gmail.com>
2022-11-14 13:53:34 +0100

    Añadiendo Inicio Sesion e Icono

--- prSceneCenec/src/application/Main.java
+++ prSceneCenec/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -13,6 +14,8 @@ public class Main extends Application {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
 			Scene scene = new Scene(root,400,400);
+			primaryStage.setTitle("Inicio de sesion");
+			primaryStage.getIcons().add(new Image("/application/casa.png"));
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
