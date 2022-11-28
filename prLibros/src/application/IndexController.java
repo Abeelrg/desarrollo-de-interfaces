cf5a6581cbfc9e34a42af96c46a7e3ef5a82487b
Abeelrg <abelriquelmegomezcenec@gmail.com>
2022-11-28 14:27:11 +0100

    Actualizacion proyecto prLibro Alertas con Errores + Borrar Tablas

--- prLibros/src/application/IndexController.java
+++ prLibros/src/application/IndexController.java
@@ -13,104 +13,107 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-
 public class IndexController {
-	
+
 	@FXML
 	private TextField txtTitulo;
-	
+
 	@FXML
 	private ChoiceBox cbEditorial;
-	
+
 	@FXML
 	private TextField txtAutor;
-	
+
 	@FXML
 	private TextField txtPaginas;
-	
+
 	@FXML
-	private TableView <Libro> tableLibros;
-	
+	private TableView<Libro> tableLibros;
+
 	@FXML
-	private TableColumn <Libro, String> columnTitulo;
-	
+	private TableColumn<Libro, String> columnTitulo;
+
 	@FXML
-	private TableColumn <Libro, String> columnEditorial;
-	
+	private TableColumn<Libro, String> columnEditorial;
+
 	@FXML
-	private TableColumn <Libro, String> columnAutor;
-	
+	private TableColumn<Libro, String> columnAutor;
+
 	@FXML
-	private TableColumn <Libro, Integer> columnPaginas;
-	
+	private TableColumn<Libro, Integer> columnPaginas;
+
 	@FXML
 	private Button btnAnadir;
-	
-	private ObservableList<Libro> listaLibros =
-		FXCollections.observableArrayList(
-				new Libro("La Biblia", "Planeta", "Jesús", 500)
-		);
-	
-	
-	public ObservableList<String> listaEditoriales = 
-		FXCollections.observableArrayList(
-			"Planeta",
-			"Altaya",
-			"Kadokawa",
-			"Penguin Libros" 
-		);
-	
+
+	private ObservableList<Libro> listaLibros = FXCollections
+			.observableArrayList(new Libro("La Biblia", "Planeta", "Jesús", 500));
+
+	public ObservableList<String> listaEditoriales = FXCollections.observableArrayList("Planeta", "Altaya", "Kadokawa",
+			"Penguin Libros");
+
 	@FXML
 	private void initialize() {
-		
+
 		cbEditorial.setItems(listaEditoriales);
-		
+
 		columnTitulo.setCellValueFactory(new PropertyValueFactory<>("titulo"));
 		columnEditorial.setCellValueFactory(new PropertyValueFactory<>("editorial"));
 		columnAutor.setCellValueFactory(new PropertyValueFactory<>("autor"));
 		columnPaginas.setCellValueFactory(new PropertyValueFactory<>("paginas"));
-		
-		tableLibros.setItems(listaLibros); 
+
+		tableLibros.setItems(listaLibros);
 	}
-	
+
 	@FXML
 	public void anadirLibro(ActionEvent event) {
-		if (esNumero(txtPaginas.getText())) {
-			Libro l = new Libro(
-					txtTitulo.getText(),
-					cbEditorial.getValue().toString(),
-					txtAutor.getText(),
-					Integer.parseInt(txtPaginas.getText())
-			);
-			
-			listaLibros.add(l);
-			
-			txtTitulo.clear();
-			cbEditorial.getSelectionModel().clearSelection();
-			txtAutor.clear();
-			txtPaginas.clear();
-		}else {
+
+		if (txtTitulo.getText().isEmpty() || txtAutor.getText().isEmpty() || cbEditorial.getValue().toString().isEmpty()
+				|| txtPaginas.getText().isEmpty()) {
 			Alert alerta = new Alert(AlertType.ERROR);
-			alerta.setTitle("Error al insertar");
-			alerta.setHeaderText("No se ha introducido un numero en las páginas");
-			alerta.setContentText("Porfavor,introduzca un numero en las páginas");
+			alerta.setTitle("Error no se ha insertado un campo");
+			alerta.setHeaderText("No se ha introducido un campo");
+			alerta.setContentText("Porfavor,no dejes ningun campo en blanco");
 			alerta.showAndWait();
-			
-			/**
-			 * SI NO SE INTRODUCEN TODOS LOS DATOS
-			 * SE MOSTRARA UNA ALERTA DE TIPO WARNING
-			 * AVISANDO DE QUE FALTAN DATOS.
-			 */
+		} else {
+			if (esNumero(txtPaginas.getText())) {
+				Libro l = new Libro(txtTitulo.getText(), cbEditorial.getValue().toString(), txtAutor.getText(),
+						Integer.parseInt(txtPaginas.getText()));
+
+				listaLibros.add(l);
+
+				txtTitulo.clear();
+				cbEditorial.getSelectionModel().clearSelection();
+				txtAutor.clear();
+				txtPaginas.clear();
+			} else {
+				Alert alerta = new Alert(AlertType.ERROR);
+				alerta.setTitle("Error al insertar");
+				alerta.setHeaderText("No se ha introducido un numero en las páginas");
+				alerta.setContentText("Porfavor,introduzca un numero en las páginas");
+				alerta.showAndWait();
+
+
+			}
+		}
+
+	}
+
+	@FXML
+	public void borrarLibro(ActionEvent event) {
+		try {
+		int indiceSeleccionado = tableLibros.getSelectionModel().getSelectedIndex();
+		tableLibros.getItems().remove(indiceSeleccionado);
+		}catch(IndexOutOfBoundsException e){
+			Alert alerta = new Alert(AlertType.ERROR);
+			alerta.setTitle("No has seleccionado ninguna tabla");
+			alerta.setHeaderText("Ninguna tabla seleccionada");
+			alerta.setContentText("Porfavor,selecciona una tabla para borrarla");
+			alerta.showAndWait();
 		}
 		
 	}
-	
-	@FXML
-	public void borrarLibro(ActionEvent event) { 
-		System.out.println("Borrando un libro");
-	}
-	
-	public boolean esNumero (String s) {
+
+	public boolean esNumero(String s) {
 		try {
 			Integer.parseInt(s);
 			return true;
@@ -118,5 +121,5 @@ public class IndexController {
 			return false;
 		}
 	}
-		
+
 }
